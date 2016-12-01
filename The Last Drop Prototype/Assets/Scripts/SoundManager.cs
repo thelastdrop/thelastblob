45d48b07d6ac4164bcc9b94e7319504c73f1ccf3
Du Lin <lnidux@gmail.com>
2016-12-01 15:14:46 +0100

    SoudManager basic class.

---
+++ The Last Drop Prototype/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed
+{
+    public class SoundManager : MonoBehaviour 
+    {
+        public AudioSource efxSource;
+        public AudioSource musicSource;
+        public static SoundManager instance = null;        
+        
+        void Awake ()
+        {
+            // Singleton pattern
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy (gameObject);
+            DontDestroyOnLoad (gameObject);
+        }
+        
+        
+        //Used to play single sound clips.
+        public void PlaySingle(AudioClip clip)
+        {
+            efxSource.clip = clip;          
+            efxSource.Play ();
+        }
+    }
+}
