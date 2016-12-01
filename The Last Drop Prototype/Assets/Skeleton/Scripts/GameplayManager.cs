bddcbb476fe08d2a4e2d01674e2f4aa043b8941e
elekit <kit93@hotmail.it>
2016-12-01 17:18:19 +0100

    Added Skeleton and Menu Scene

---
+++ The Last Drop Prototype/Assets/Skeleton/Scripts/GameplayManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using POLIMIGameCollective;
+
+public class GameplayManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MusicManager.Instance.PlayMusic ("GameplayMusic");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			SfxManager.Instance.Play ("creature");
+		else if (Input.GetKeyDown (KeyCode.Alpha2))
+			SfxManager.Instance.Play ("jump");
+		else if (Input.GetKeyDown (KeyCode.Alpha3))
+			SfxManager.Instance.Play ("laser");
+		else if (Input.GetKeyDown (KeyCode.Alpha4))
+			SfxManager.Instance.Play ("lose");
+		else if (Input.GetKeyDown (KeyCode.Alpha5))
+			SfxManager.Instance.Play ("pickup");
+		else if (Input.GetKeyDown (KeyCode.Alpha6))
+			SfxManager.Instance.Play ("radar");
+		else if (Input.GetKeyDown (KeyCode.Alpha7))
+			SfxManager.Instance.Play ("rumble");
+		else if (Input.GetKeyDown (KeyCode.Space)) {
+			MusicManager.Instance.StopAll ();
+			MusicManager.Instance.PlayMusic ("MenuMusic");
+			SceneManager.LoadScene ("Menu");
+		}
+	}
+}
---
+++ The Last Drop Prototype/Assets/Skeleton/Scripts/MusicManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace POLIMIGameCollective {
+	
+	public class MusicManager : Singleton<MusicManager> {
+
+		private AudioSource[] m_audio_sources;
+
+		// true if using the game object name to start the music
+		public bool m_useGONames = false;
+
+		private Dictionary<string, AudioSource> music_list;
+
+		void Awake() {
+			if (FindObjectsOfType (typeof(MusicManager)).Length > 1) {
+				Destroy (gameObject);
+			} else {
+				DontDestroyOnLoad (gameObject);
+			}
+
+			music_list = new Dictionary<string, AudioSource>();
+
+			m_audio_sources = gameObject.GetComponentsInChildren<AudioSource>();
+
+			for (int i = 0; i < m_audio_sources.Length; i++) {
+				AudioSource s = m_audio_sources [i];
+				if (m_useGONames)
+					music_list [s.gameObject.name] = s;
+				else
+					music_list [s.clip.name] = s;
+			}
+		}
+
+		public void PlayMusic(string name, float pitchVariance = 0)
+		{
+			if (music_list.ContainsKey(name))
+			{
+				if (pitchVariance != 0) music_list[name].pitch = 1 + Random.Range(-pitchVariance, pitchVariance);
+
+				if (!music_list [name].isPlaying)
+					music_list[name].Play();
+
+			} else Debug.LogWarning("No sound of name " + name + " exists");
+		}
+
+		public void MuteAll()
+		{
+			for (int i = 0; i < m_audio_sources.Length; i++)
+				m_audio_sources [i].mute = true;
+		}
+
+		public void UnmuteAll()
+		{
+			for (int i = 0; i < m_audio_sources.Length; i++)
+				m_audio_sources [i].mute = false;
+		}
+
+		public void StopAll()
+		{
+			for (int i = 0; i < m_audio_sources.Length; i++)
+				m_audio_sources [i].Stop ();
+		}
+
+		public bool isPlaying(string name) {
+			if (music_list.ContainsKey (name)) {
+				return music_list [name].isPlaying;
+			} else {
+				Debug.LogWarning ("No sound of name " + name + " exists");
+				return false;
+			}
+		}
+			
+	}
+}
