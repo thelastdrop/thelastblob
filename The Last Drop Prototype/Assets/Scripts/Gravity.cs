2932f36c45ba65c9f821dfbccaa976b58a88ee36
Du Lin <lnidux@gmail.com>
2016-12-05 18:10:40 +0100

    changed PlayerSettings to build Android .apk and testing touch input (Gravity.cs).

--- The Last Drop Prototype/Assets/Scripts/Gravity.cs
+++ The Last Drop Prototype/Assets/Scripts/Gravity.cs
@@ -7,6 +7,8 @@ public class Gravity : MonoBehaviour {
 	public float delay = 2f;
 	public float gravityValue = 9.81f;
 	Vector3 gravity;
+
+	private Vector2 touchOrigin = -Vector2.one;	
  
 	void Start() {
 		gravity = Physics2D.gravity;
@@ -14,9 +16,8 @@ public class Gravity : MonoBehaviour {
    		nextUsage = Time.time + delay;
  	}
 
-	void Update () {
+	void Update() {
 		changeGravity();
-
 	}
 
 	void FixedUpdate() {
@@ -27,29 +28,63 @@ public class Gravity : MonoBehaviour {
 		
 		#if UNITY_STANDALONE || UNITY_WEBPLAYER // Unity3D editor or web player
 
-		if(Input.GetKeyDown (KeyCode.W) && Time.time > nextUsage) {
+		if (Input.GetKeyDown (KeyCode.W) && Time.time > nextUsage) {
      		nextUsage = Time.time + delay;
 			gravity = new Vector3(0f, gravityValue, 0f);
 		}
-		if(Input.GetKeyDown (KeyCode.A) && Time.time > nextUsage) {
+		if (Input.GetKeyDown (KeyCode.A) && Time.time > nextUsage) {
      		nextUsage = Time.time + delay;
 			gravity = new Vector3(-gravityValue, 0f, 0f);
 		}
-		if(Input.GetKeyDown (KeyCode.S) && Time.time > nextUsage) {
+		if (Input.GetKeyDown (KeyCode.S) && Time.time > nextUsage) {
      		nextUsage = Time.time + delay;
 			gravity = new Vector3(0f, -gravityValue, 0f);			
 		}
-		if(Input.GetKeyDown (KeyCode.D) && Time.time > nextUsage) {
+		if (Input.GetKeyDown (KeyCode.D) && Time.time > nextUsage) {
      		nextUsage = Time.time + delay;
 			gravity = new Vector3(gravityValue, 0f, 0f);
-			
 		}
-		
+
 		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE // mobile controls
+
+		int horizontal = 0;
+		int vertical = 0;
+
+		if (Input.touchCount > 0) {
+        	Touch myTouch = Input.touches[0];
+        	
+			if (myTouch.phase == TouchPhase.Began) {
+            	touchOrigin = myTouch.position;
+            } else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
+            	Vector2 touchEnd = myTouch.position;                    
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+
+                touchOrigin.x = -1;
+			if (Mathf.Abs(x) > Mathf.Abs(y))
+            	horizontal = x > 0 ? 1 : -1;
+        	else
+				vertical = y > 0 ? 1 : -1;
+            }
+        }
+
+		if (vertical > 0 && Time.time > nextUsage) {
+     		nextUsage = Time.time + delay;
+			gravity = new Vector3(0f, gravityValue, 0f);
+		}
+		if (horizontal < 0 && Time.time > nextUsage) {
+     		nextUsage = Time.time + delay;
+			gravity = new Vector3(-gravityValue, 0f, 0f);
+		}
+		if (vertical < 0 && Time.time > nextUsage) {
+     		nextUsage = Time.time + delay;
+			gravity = new Vector3(0f, -gravityValue, 0f);			
+		}
+		if (horizontal > 0 && Time.time > nextUsage) {
+     		nextUsage = Time.time + delay;
+			gravity = new Vector3(gravityValue, 0f, 0f);	
+		}
 
 		#endif
 	}
-
-
-
-}
+}		
