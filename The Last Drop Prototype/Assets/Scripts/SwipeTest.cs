b1cadba6e7a6212a38c124d121da9d280417ee48
Du Lin <lnidux@gmail.com>
2016-12-06 19:26:56 +0100

    clean + comments in SwipeTest.cs

--- The Last Drop Prototype/Assets/Scripts/SwipeTest.cs
+++ The Last Drop Prototype/Assets/Scripts/SwipeTest.cs
@@ -6,17 +6,13 @@ public class SwipeTest : MonoBehaviour {
 
 	Transform tr;
 	private Vector2 touchOrigin = -Vector2.one;
-	private RaycastHit2D hit;
-	private float x = 0f;
-	private float y = 0f;
-	float speed = .03f;
+	private Vector2 swipeVector; // Vector representing swipe input
+	public float speed = .03f;
 
-	// Use this for initialization
 	void Start () {
 		tr = GetComponent<Transform>() as Transform;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE // mobile controls
 
@@ -25,22 +21,20 @@ public class SwipeTest : MonoBehaviour {
         	
 			if (myTouch.phase == TouchPhase.Began) {
             	touchOrigin = myTouch.position;
-				hit = Physics2D.Raycast (touchOrigin, tr.position, .1f);
-				Debug.Log("inside if");
-            } else if (/*hit &&*/ myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
+            } else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
             	Vector2 touchEnd = myTouch.position;                    
-                x = touchEnd.x - touchOrigin.x;
-                y = touchEnd.y - touchOrigin.y;
+                swipeVector = new Vector2(touchEnd.x - touchOrigin.x, touchEnd.y - touchOrigin.y);
 				//float dist = Vector2.Distance(touchOrigin,touchEnd);
-				Debug.Log("inside else if");
+
                 touchOrigin.x = -1;
-            }  
+            }
         }
 
 		#endif
 	}
 
 	void FixedUpdate() {
-		tr.position = tr.position + x * transform.right * speed * Time.fixedDeltaTime + y * transform.up * speed * Time.fixedDeltaTime;
+		// Moving a GameObject test
+		tr.position = tr.position + swipeVector.x * transform.right * speed * Time.fixedDeltaTime + swipeVector.y * transform.up * speed * Time.fixedDeltaTime;
 	}
 }
