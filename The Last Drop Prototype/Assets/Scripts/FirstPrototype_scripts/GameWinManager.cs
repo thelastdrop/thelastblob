19a61caea9752b5934cae7b706a5150f2541c9f9
elekit <kit93@hotmail.it>
2016-12-07 15:03:12 +0100

    Clean up and documentation

--- The Last Drop Prototype/Assets/Scripts/FirstPrototype_scripts/GameWinManager.cs
+++ The Last Drop Prototype/Assets/Scripts/FirstPrototype_scripts/GameWinManager.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using POLIMIGameCollective;
 
-public class GameWinManager : MonoBehaviour
+// This class manages all the game architecture, loading a level, pausing,  restarting, winning, losing
+
+public class GameWinManager : Singleton<GameWinManager>
 {
-	
-	public static GameWinManager Instance = null;
+
+	// index of current level
 	private int current_level = 0;
-	private int tutorial = 0;
+
+
+	// index of the first level accesible by the player at the first opening
+	private const int tutorial = 0;
 
 
 	[Header ("EndLevel Screen")]
 	public GameObject m_endlevel_screen;
-	public Text m_ending_text;
 
 
 	[Header ("LoseLevel Screen")]
@@ -23,63 +28,58 @@ public class GameWinManager : MonoBehaviour
 	[Header ("PauseLevel Screen")]
 	public GameObject m_pauselevel_screen;
 
-	//[Header("Gameplay Screen")]
-	//public GameObject m_timer_screen;
-	//public Text m_timer_text;
 
 	[Header ("Choose-Levels Screen")]
 	public GameObject m_levels_screen;
 
 
+
+	/* array of gameplay screens, each index is one level, this objects are never used directly nor mutated,
+	 * but cloned (Object.Instantiate) in m_playing_screen each time a new level is started to ensure a clean state */
 	[Header ("Gameplay Screens")]
 	public GameObject[] m_gameplay_screens;
 
+	private GameObject m_playing_screen;
+
+	//if timer is implemented use this
 	[Range (0f, 4f)]
 	private float m_current_time = 0;
 
 	public float CurrentTime { get { return m_current_time; } }
 
-
-	[Header ("Levels")]
-	public Level[] m_levels;
+	/* array of booleans with the same length of levels and same indexes, if a level is playable because the gamers 
+	 * won it the m_levels_accessible[i] is true otherwise is false and that level isn't accessible */
 
 	[Header ("Levels accessible")]
 	public bool[] m_levels_accessible;
 
 
+	[Header ("Loading time for Level")]
 	[Range (0f, 4f)]
 	public float m_loading_time = 0.5f;
+
 
 	[Header ("Gravity Input")]
 	public Gravity gravityInput;
 
 
-	private GameObject m_playing_screen;
 
-	void Awake ()
-	{
-		if (Instance == null) {
-			Instance = this;
-		} else {
-			Destroy (gameObject);
-		}
-	}
-
-
-	// Use this for initialization
 	void Start ()
 	{
-		//set all the levels except the first non accessible
+		//set all the levels except the first as not accessible
 		for (int i = 0; i < m_gameplay_screens.Length; i++) {
 			if (i == tutorial) {
 				m_levels_accessible [i] = true;
 			} else {
 				m_levels_accessible [i] = false;
 			}
+			//deactivate all the level screens, they will never be used directly 
 			m_gameplay_screens [i].SetActive (false);
 		}
+
 		ClearScreens ();
 
+		// activate level chooser
 		m_levels_screen.SetActive (true);
 
 	}
@@ -98,7 +98,7 @@ public class GameWinManager : MonoBehaviour
 
 
 
-	//bind this with the buttons of every level
+	//called when the user choses one level
 	public void ChooseLevel (int n)
 	{
 		if (m_levels_accessible [n]) {
@@ -111,6 +111,7 @@ public class GameWinManager : MonoBehaviour
 
 
 
+	// loads the current level
 
 	IEnumerator LoadLevel ()
 	{
@@ -121,6 +122,8 @@ public class GameWinManager : MonoBehaviour
 		this.ClearScreens ();
 		gravityInput.ResetGravity ();
 
+
+		//duplicate the required level and activate it
 		m_playing_screen = Instantiate (m_gameplay_screens [current_level]);
 		m_playing_screen.SetActive (true);
 	
@@ -128,7 +131,7 @@ public class GameWinManager : MonoBehaviour
 	}
 
 
-	//use with the button play next level
+	//triggered by the button "next level"
 	public void NextLevel ()
 	{
 		current_level++;
@@ -138,15 +141,17 @@ public class GameWinManager : MonoBehaviour
 
 
 
-	//use with the button play again
+	//triggered by the button "play again"
 	public void ReloadLevel ()
 	{
 		StartCoroutine (LoadLevel ());
 	}
 
 
+	//called when the player reaches the end of the level
 	public void WinLevel ()
 	{
+		// set as accessible (true) the next level if the current one is won
 		if (current_level + 1 < m_levels_accessible.Length) {
 			m_levels_accessible [current_level + 1] = true;
 		}
@@ -154,36 +159,44 @@ public class GameWinManager : MonoBehaviour
 		m_endlevel_screen.SetActive (true);
 
 	}
-		
 
-	void EndLevel()
+
+	// called to destroy the current level screen
+	// never called directly by the UI
+	void EndLevel ()
 	{
 		this.ClearScreens ();
+		// destroy the currently allocated level screen when a level ends winning/losing
 		Destroy (m_playing_screen);
 		
 	}
 
 
-	public void LoseLevel()
+	//called when the player loses in a level
+	public void LoseLevel ()
 	{
 		this.EndLevel ();
 		m_loselevel_screen.SetActive (true);
 	}
 
 
-	public void PauseLevel()
+	//called when the player pauses the game
+	public void PauseLevel ()
 	{
 		m_playing_screen.SetActive (false);
 		m_pauselevel_screen.SetActive (true);
 	}
 
 
-	public void ResumeLevel(){
+	//triggered by the button "continue" in the pause screen
+	public void ResumeLevel ()
+	{
 		m_pauselevel_screen.SetActive (false);
 		m_playing_screen.SetActive (true);
 	}
-		
 
+
+	// deactivate all the screens
 	void ClearScreens ()
 	{
 		if (m_endlevel_screen != null)
@@ -201,7 +214,8 @@ public class GameWinManager : MonoBehaviour
 
 
 
-	public void SwitchToMenu()
+	//come back to main menu
+	public void SwitchToMenu ()
 	{
 		SceneManager.LoadScene ("Menu");
 	}
--- The Last Drop Prototype/Assets/Scripts/Gravity.cs
+++ The Last Drop Prototype/Assets/Scripts/Gravity.cs
@@ -1,44 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gravity : MonoBehaviour {
+public class Gravity : MonoBehaviour
+{
 
 	float nextUsage;
 	public float delay = 2f;
 	public float gravityValue = 9.81f;
- 
-	void Start() {
-		ResetGravity ();
-   		nextUsage = Time.time + delay;
- 	}
 
-	void Update () {
-		changeGravity();
+	void Start ()
+	{
+		ResetGravity ();
+		nextUsage = Time.time + delay;
 	}
 
-	public void ResetGravity() {
+	void Update ()
+	{
+		changeGravity ();
+	}
+
+	// Reset the gravity to point down
+	public void ResetGravity ()
+	{
 		Physics2D.gravity = new Vector3 (0f, -gravityValue, 0f);
 	}
 
-	void changeGravity() {
+	void changeGravity ()
+	{
 		
 		#if UNITY_STANDALONE || UNITY_WEBPLAYER // Unity3D editor or web player
 
-		if(Input.GetKeyDown (KeyCode.W) && Time.time > nextUsage) {
-     		nextUsage = Time.time + delay;
-			Physics2D.gravity = new Vector3(0f, gravityValue, 0f);
+		if (Input.GetKeyDown (KeyCode.W) && Time.time > nextUsage) {
+			nextUsage = Time.time + delay;
+			Physics2D.gravity = new Vector3 (0f, gravityValue, 0f);
 		}
-		if(Input.GetKeyDown (KeyCode.A) && Time.time > nextUsage) {
-     		nextUsage = Time.time + delay;
-			Physics2D.gravity = new Vector3(-gravityValue, 0f, 0f);
+		if (Input.GetKeyDown (KeyCode.A) && Time.time > nextUsage) {
+			nextUsage = Time.time + delay;
+			Physics2D.gravity = new Vector3 (-gravityValue, 0f, 0f);
 		}
-		if(Input.GetKeyDown (KeyCode.S) && Time.time > nextUsage) {
-     		nextUsage = Time.time + delay;
-			Physics2D.gravity = new Vector3(0f, -gravityValue, 0f);			
+		if (Input.GetKeyDown (KeyCode.S) && Time.time > nextUsage) {
+			nextUsage = Time.time + delay;
+			Physics2D.gravity = new Vector3 (0f, -gravityValue, 0f);			
 		}
-		if(Input.GetKeyDown (KeyCode.D) && Time.time > nextUsage) {
-     		nextUsage = Time.time + delay;
-			Physics2D.gravity = new Vector3(gravityValue, 0f, 0f);
+		if (Input.GetKeyDown (KeyCode.D) && Time.time > nextUsage) {
+			nextUsage = Time.time + delay;
+			Physics2D.gravity = new Vector3 (gravityValue, 0f, 0f);
 		}
 		
 		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE // mobile controls
