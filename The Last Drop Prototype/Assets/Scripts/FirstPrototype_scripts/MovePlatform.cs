ab5c589ba35363573aaf7e22c3a262b2ea89b970
elekit <kit93@hotmail.it>
2016-12-10 17:01:48 +0100

    Fix moving platform

--- The Last Drop Prototype/Assets/Scripts/FirstPrototype_scripts/MovePlatform.cs
+++ The Last Drop Prototype/Assets/Scripts/FirstPrototype_scripts/MovePlatform.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class MovePlatform : MonoBehaviour
 {
 
-	HingeJoint2D joint;
+	private HingeJoint2D joint;
+	private JointMotor2D motor;
+	private float motorSpeed;
 
 	// Use this for initialization
 	void Start ()
 	{
 		joint = GetComponent<HingeJoint2D> () as HingeJoint2D;
+		motor = joint.motor;
+		motorSpeed = motor.motorSpeed;
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (joint.limitState == JointLimitState2D.LowerLimit || joint.limitState == JointLimitState2D.UpperLimit) {
-			JointMotor2D motor = joint.motor;
-			motor.motorSpeed = -motor.motorSpeed;
+		if (joint.limitState == JointLimitState2D.UpperLimit) {
+			motor.motorSpeed = motorSpeed;
+			joint.motor = motor;
+		} else if (joint.limitState == JointLimitState2D.LowerLimit) {
+			motor.motorSpeed = -motorSpeed;
 			joint.motor = motor;
 		}
 	}
