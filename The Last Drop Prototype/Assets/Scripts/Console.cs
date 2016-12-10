af1b75a585190d7559503a71d6af3ce74d280148
eymrich <kuurstra.renato@gmail.com>
2016-12-10 14:08:47 +0100

    console scripts
    console scripts and inteface

---
+++ The Last Drop Prototype/Assets/Scripts/Console.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : MonoBehaviour {
+
+    public GameObject[] Object_Linked;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
