90a301853b8094e37cd89822fd6f613363fc9faa
Du Lin <lnidux@gmail.com>
2016-12-10 16:29:38 +0100

    Updated TouchControlManager.

--- The Last Drop Prototype/Assets/Scripts/TouchControlManager.cs
+++ The Last Drop Prototype/Assets/Scripts/TouchControlManager.cs
@@ -11,7 +11,9 @@ public class TouchControlManager : Singleton<TouchControlManager> {
 	// [Left portion] Move info 
 	public Vector2 moveStartPos;
 	public Vector2 moveDirection;
+	private bool moving;
 	private float maxMovRange;
+	private float shrinkVectorFactor = 0.01f;
 	
 	// [Right portion] Vector representing swipe input
 	public Vector2 swipeVector;
@@ -22,7 +24,7 @@ public class TouchControlManager : Singleton<TouchControlManager> {
 		leftR = new Rect(0f, 0f, Screen.width * 0.5f, Screen.height);
     	rightR = new Rect(Screen.width * 0.5f, 0f, Screen.width * 0.5f, Screen.height);
 		
-		maxMovRange = Screen.width * 0.1f;
+		maxMovRange = Screen.width * 0.05f;
 	}
 
 	void Update () {
@@ -32,7 +34,7 @@ public class TouchControlManager : Singleton<TouchControlManager> {
 	void TouchInputTrigger() {
 #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE // mobile controls
 		// reset value each frame
-		swipeVector = Vector2.zero;
+		// swipeVector = Vector2.zero;
 
 		if (Input.touchCount > 0) {
 			
@@ -42,29 +44,35 @@ public class TouchControlManager : Singleton<TouchControlManager> {
 			if(leftR.Contains(touch.position)) {
 				switch(touch.phase) {
 					case TouchPhase.Began:
+						moving = true;
 						moveStartPos = touch.position;
 						EventManager.TriggerEvent("MoveStart");
 						break;
 					case TouchPhase.Moved:
+						moving = true;
 						Vector2 tempDirection = touch.position - moveStartPos;
-						moveDirection = tempDirection.magnitude > maxMovRange ? tempDirection.normalized * maxMovRange : tempDirection;
+						moveDirection = tempDirection.magnitude > maxMovRange ? tempDirection.normalized * maxMovRange * shrinkVectorFactor : tempDirection  * shrinkVectorFactor;
 						break;
 					case TouchPhase.Ended:
+						moving = false;
 						EventManager.TriggerEvent("MoveEnd");
 						break;
 				}		
-            }
-
-			// Right portion of the screen: stretch mechanic
-        	if(rightR.Contains(touch.position)) {
+            }	// Right portion of the screen: stretch mechanic
+        	else if(rightR.Contains(touch.position)) {
 				if (touch.phase == TouchPhase.Began) {
             		touchOrigin = touch.position;
-            	} else if (touch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
+            	} else if (touch.phase == TouchPhase.Ended) {
             		Vector2 touchEnd = touch.position;                    
-                	swipeVector = new Vector2(touchEnd.x - touchOrigin.x, touchEnd.y - touchOrigin.y);
-					touchOrigin.x = -1;
+                	swipeVector = touchEnd - touchOrigin;
+					// touchOrigin.x = -1;
 					// Trigger event: i.e. swipeVector has changed 
 					EventManager.TriggerEvent("Swipe");
+
+					if(moving) {
+						moving = false;
+						EventManager.TriggerEvent("MoveEnd");
+					}
             	}
 			}
         }
