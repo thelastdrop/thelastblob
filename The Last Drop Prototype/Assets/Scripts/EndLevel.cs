128feca3f417f2841a4514f3c8b759c94c24c6d6
eymrich <kuurstra.renato@gmail.com>
2016-12-12 22:28:23 +0100

    awd
    dawdwa

--- The Last Drop Prototype/Assets/Scripts/EndLevel.cs
+++ The Last Drop Prototype/Assets/Scripts/EndLevel.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class EndLevel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         if( collision.gameObject.tag == "Player" )
