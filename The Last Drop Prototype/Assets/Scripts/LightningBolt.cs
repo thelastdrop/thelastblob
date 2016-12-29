cb452ea2ecb68f4d9824e69a8ba0bd7056370332
Du Lin <lnidux@gmail.com>
2016-12-29 14:29:21 +0100

    Added TeslaCoil shader effect

---
+++ The Last Drop Prototype/Assets/Scripts/LightningBolt.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class LightningBolt : MonoBehaviour
+{
+	
+	private Renderer renderer;
+
+	void Start ()
+	{
+		renderer = GetComponent<Renderer>();		
+		Material newMat = renderer.material;
+		newMat.SetFloat("_StartSeed",Random.value*1000);
+		renderer.material = newMat;
+		
+	}
+
+}
+
