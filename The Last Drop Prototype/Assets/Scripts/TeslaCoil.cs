f7a32ba9515518128f0d041ef0b9f0ea6ae62d35
Du Lin <lnidux@gmail.com>
2017-01-03 10:22:33 +0100

    Created TeslaCoil.cs: calls Deactivate_Particle()

--- The Last Drop Prototype/Assets/Scripts/TeslaCoil.cs
+++ The Last Drop Prototype/Assets/Scripts/TeslaCoil.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class TeslaCoil : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			GameManager.Instance.m_Player.GetComponent<PlayerAvatar_02> ().Deactivate_Particle (collision.gameObject);
+		}
 	}
 }
