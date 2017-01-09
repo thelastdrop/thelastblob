2e6f293fa266f95c631617b78c54330e4089265e
eymrich <kuurstra.renato@gmail.com>
2017-01-09 18:43:41 +0100

    various things

--- The Last Drop Prototype/Assets/Scripts/SoundManager.cs
+++ The Last Drop Prototype/Assets/Scripts/SoundManager.cs
@@ -13,6 +13,9 @@ public class SoundManager : Singleton<SoundManager>
     [Header("Music sound")]
     public AudioClip m_Music_Loop;
 
+    [Header("Blob Sound")]
+    public AudioClip m_Blob_Loop;
+
     public float lowPitchRange = 0.95f;
     public float highPitchRange = 1.05f;
 
@@ -25,6 +28,8 @@ public class SoundManager : Singleton<SoundManager>
     [Tooltip("Maximum absolute distance at which player ear the sound"),Header("Global Sound Variable"), Space(5)]
     public float m_Max_Distance = 50f;
     public AnimationCurve m_Sound_Falloff;
+
+    private bool m_IsTime_Frozen = true;
 
     //Awake
     public void Start()
@@ -40,7 +45,24 @@ public class SoundManager : Singleton<SoundManager>
         // 6  --- Screaming and deaths
         m_AudioS = GetComponents<AudioSource>();
         PlayMusic();
+//        PlayBlob();
     }
+/*
+    void Update()
+    {
+        if(Time.timeScale == 0.0f && m_IsTime_Frozen == false)
+        {
+            PauseBlob();
+            m_IsTime_Frozen = true;
+        }
+
+        if(Time.timeScale != 0.0f && m_IsTime_Frozen == true)
+        {
+            m_IsTime_Frozen = false;
+            PlayAgainBlob();
+        }
+    }
+*/
 
     //Play the music(channel 0)
     public void PlayMusic()
@@ -62,6 +84,21 @@ public class SoundManager : Singleton<SoundManager>
     public void PlayBlobDamage()
     {
         play_sound(3, m_Blob_Damage[Random.Range(0, m_Blob_Damage.Length - 1)], true, 0f);
+    }
+
+    public void PlayBlob()
+    {
+        play_sound( 4, m_Blob_Loop, false, 0f);
+    }
+
+    public void PauseBlob()
+    {
+        m_AudioS[4].Pause();
+    }
+
+    public void PlayAgainBlob()
+    {
+        m_AudioS[4].Play();
     }
 
     public void PlayLevelSound( AudioClip clip, bool rnd_pitch, float distance)
@@ -96,6 +133,12 @@ public class SoundManager : Singleton<SoundManager>
             m_AudioS[source].volume = m_Master_Volume * m_Sfx_Volume;
         }
 
+        if (source == 4 )
+        {
+            m_AudioS[source].loop = true;
+            m_AudioS[source].volume = m_Master_Volume * m_Sfx_Volume * 0.4f;
+        }
+
         if(rnd_pitch == true)
         {
             float randomPitch = Random.Range(lowPitchRange, highPitchRange);
@@ -110,7 +153,7 @@ public class SoundManager : Singleton<SoundManager>
             float curve_time = Mathf.Clamp((m_Max_Distance - distance), 0f, m_Max_Distance) / m_Max_Distance;
             m_AudioS[source].volume *= m_Sound_Falloff.Evaluate(curve_time);
 
-            Debug.Log("Distance: " + distance + "Volume:" + m_AudioS[source].volume);
+//            Debug.Log("Distance: " + distance + "Volume:" + m_AudioS[source].volume);
         }
         
         m_AudioS[source].Play();
