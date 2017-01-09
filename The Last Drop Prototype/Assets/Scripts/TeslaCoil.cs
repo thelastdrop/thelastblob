3421f0fd62dfe19fff330ca13b6e484ff28e5efb
eymrich <kuurstra.renato@gmail.com>
2017-01-09 22:33:05 +0100

    vrious things

--- The Last Drop Prototype/Assets/Scripts/TeslaCoil.cs
+++ The Last Drop Prototype/Assets/Scripts/TeslaCoil.cs
@@ -6,20 +6,17 @@ public class TeslaCoil : MonoBehaviour {
 
 	public AudioClip[] audioclips;
 
-    public float m_CD = 0.016f;
+    public float m_CD = 0.0f;
     private float m_last_time;
 
 	void OnTriggerEnter2D (Collider2D collider)
 	{
-        if ((Time.time - m_last_time) > m_CD)
-        {
             if (collider.gameObject.tag == "Player")
             {
-                GameManager.Instance.m_Player.GetComponent<PlayerAvatar_02>().Deactivate_Particle( 2 );
+                GameManager.Instance.m_Player.GetComponent<PlayerAvatar_02>().Deactivate_Particle( collider.gameObject );
                 int randomClipIndex = Random.Range(0, audioclips.Length - 1);
                 SoundManager.Instance.PlayModPitch(audioclips[randomClipIndex]);
             }
-            m_last_time = Time.time;
-        }
+  //          m_last_time = Time.time;
 	}
 }
