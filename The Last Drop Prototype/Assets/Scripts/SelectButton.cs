3ee075c7b45a9b50c263dd756030833a94f1c68a
eymrich <kuurstra.renato@gmail.com>
2017-02-03 17:35:30 +0100

    UIAndSelectButton

---
+++ The Last Drop Prototype/Assets/Scripts/SelectButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectButton : MonoBehaviour {
+
+    public Button Button;
+
+    private void OnEnable()
+    {
+        selectit();
+    }
+
+    void selectit()
+    {
+        Button.Select();
+    }
+}
